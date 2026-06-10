c578b8e0f1bd8f8df4a5fc3408c6bb73c47b76a8
natalya finaeva <nota999999999@gmail.com>
2026-06-10 14:56:16 +0430

    Add dashboard, card profile and geo monitoring

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module project
 
 go 1.26.1
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	golang.org/x/image v0.41.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
-	"net"
 	"net/http"
 	"strconv"
+	"text/template"
+	"time"
 
+	"github.com/fogleman/gg"
 	"github.com/gorilla/mux"
 )
 
@@ -30,6 +33,43 @@ var clients = []Client{
 	{Name: "Bezhan Sh", MobileN: "+992000444444", Card: "4276666655554444"},
 	{Name: "Guldofarin Kh", MobileN: "+992000555555", Card: "6211777766665555"},
 }
+var transactions = []Transaction{
+
+	{ID: 1, Amount: 150.75, Currency: "TJS", PAN: "5440333322221111",
+		Sender: "Nozim G", Provider: "Alif", Status: "SUCCESS",
+		IP: "192.168.1.10", Country: "Tajikistan"},
+	{ID: 2, Amount: 500.00, Currency: "USD", PAN: "4000444433332222",
+		Sender: "John Tomson", Provider: "Wirecard", Status: "DECLINE",
+		IP: "185.44.77.21", Country: "Germany"},
+	{ID: 3, Amount: 320.00, Currency: "TJS", PAN: "4111555544443333",
+		Sender: "Alijon R.", Provider: "Wallet", Status: "SUCCESS",
+		IP: "46.101.12.55", Country: "UAE"},
+	{ID: 4, Amount: 89.99, Currency: "TJS", PAN: "4276666655554444",
+		Sender: "Van Li", Provider: "Binance", Status: "DECLINE",
+		IP: "8.8.8.8", Country: "USA"},
+	{ID: 5, Amount: 75.99, Currency: "EUR", PAN: "6211777766665555",
+		Sender: "N/A", Provider: "PayPal", Status: "SUCCESS",
+		IP: "185.4.4.4", Country: "China"},
+}
+var geoPoints = []GeoPoint{
+	{
+		Country: "Tajikistan",
+		IP:      "185.1.1.1",
+	},
+	{
+		Country: "Germany",
+		IP:      "185.2.2.2",
+	},
+	{
+		Country: "USA",
+		IP:      "185.3.3.3",
+	},
+	{
+		Country: "China",
+		IP:      "185.4.4.4",
+	},
+}
+var lastAlert string
 
 type CardIdentifyModel struct {
 	PAN string `json:"pan"`
@@ -38,28 +78,37 @@ type ReceivedData struct {
 	PAN string `json:"pan"`
 }
 type Transaction struct {
-	ID       int     `json:"id"`           //serial
-	Amount   float64 `json:"money_amount"` //number
-	Currency string  `json:"currency"`     //text (TJS, USD)
-	PAN      string  `json:"pan"`
-	Receiver string  `json:"receiver_name"` //text
-	Sender   string  `json:"sender_name"`   //text (Name)
-	Provider string  `json:"host"`          //text (Binance)
-	Status   string  `json:"status"`        //text (Success/Decline)
-	Message  string  `json:"message"`       //text (...)
-	Token    string  `json:"token"`
-	IP       string  `json:"ip"` //ip country
+	ID            int       `json:"id"`           //serial
+	Amount        float64   `json:"money_amount"` //number
+	Currency      string    `json:"currency"`     //text (TJS, USD)
+	PAN           string    `json:"pan"`
+	Receiver      string    `json:"receiver_name"` //text
+	Sender        string    `json:"sender_name"`   //text (Name)
+	Provider      string    `json:"host"`          //text (Binance)
+	Status        string    `json:"status"`        //text (Success/Decline)
+	Message       string    `json:"message"`       //text (...)
+	Token         string    `json:"token"`
+	IP            string    `json:"ip"` //ip country
+	Country       string    `json:"country"`
+	Alert         string    `json:"alert"`
+	Time          time.Time `json:"time"`
+	TimeFormatted string    `json:"time_formatted"`
 }
-
+type GeoPoint struct {
+	Country string `json:"country"`
+	IP      string `json:"ip"`
+	Status  string `json:"status"`
+}
 type GeoLocation struct {
 	Country string `json:"country"`
 }
 
 type TransactionResponse struct {
-	Token   string `json:"token"`
 	Status  string `json:"status"`
+	Token   string `json:"token"`
 	Message string `json:"message"`
 }
+
 type Sender struct {
 	SenderName string `json:"sender_name"`
 	Provider   string `json:"sender_host"`
@@ -81,16 +130,48 @@ type CardInfo struct {
 	ClientName string `json:"client_name"`
 	Phone      string `json:"mobile"`
 }
+type CardPage struct {
+	CardInfo CardInfo
+	History  []Transaction
+}
+type Dashboard struct {
+	FraudScore   int
+	Velocity     string
+	Transactions int
+	TxList       []Transaction
+	GeoPoints    []GeoPoint
+
+	Alert string
+	PAN   string
+	Token string
+}
 
 func main() {
+
 	r := mux.NewRouter()
+
+	// API routes
 	r.HandleFunc("/card", CardIdentify).Methods("POST")
 	r.HandleFunc("/format", CardFormat).Methods("POST")
 	r.HandleFunc("/record", TransactionsRecord).Methods("POST")
-	r.HandleFunc("/data", CardData).Methods("POST")
+	r.HandleFunc("/data", CardData).Methods("GET")
 
-	fmt.Println("List 8080")
-	http.ListenAndServe(":8080", r)
+	// Dashboard
+	r.HandleFunc("/dashboard", DashboardHandler).Methods("GET")
+
+	// STATIC FILES (карта, css, изображения)
+	r.PathPrefix("/static/").Handler(
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("./static")),
+		),
+	)
+
+	fmt.Println("STATIC PATH SHOULD BE: ./static")
+	fmt.Println("Dashboard: http://localhost:8080/dashboard")
+	fmt.Println("Static: http://localhost:8080/static/map.png")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+
 }
 
 func CardIdentify(w http.ResponseWriter, r *http.Request) {
@@ -134,47 +215,48 @@ func TransactionsRecord(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	transaction.Time = time.Now()
+
+	transaction.TimeFormatted = transaction.Time.Format("2006-01-02 15:04:05")
+
 	fmt.Println("Transaction:", transaction)
 
 	//тут проверка антифрода и результат в терминал
 	if CheckDroppers(transaction) {
 
-		//тут сохранён оригинальный PAN
-		originalPAN := transaction.PAN
+		transaction.Status = "DECLINE"
+		transaction.Alert = "⚠ DROP DETECTED"
+		transaction.Message = "Неправильный номер карты"
 
-		//это вывод в терминал
+		lastAlert = "⚠ DROP ATTACK DETECTED"
+
+		// оригинальный PAN
+		originalPAN := transaction.PAN
 		fmt.Println("Original PAN:", originalPAN)
 
-		//тут токенизация
+		// генерация токена
 		var token string
-
 		for i := 0; i < 16; i++ {
 			token += strconv.Itoa(rand.Intn(10))
 		}
 
-		//тут вывод токена и уведомление в терминал
 		fmt.Println("Generated token:", token)
 
-		fmt.Println("Попытка дроп платежа! Проверить.")
+		// alert
+		alert := "Попытка Дроп Платежа! Проверить."
+		fmt.Println(alert)
 
-		//тут Геомонитринг в терминал
+		// маскировка
 
-		ip := transaction.IP
+		maskedToken := MaskToken(token)
 
-		host, _, err := net.SplitHostPort(r.RemoteAddr)
-		if err != nil {
-			host = r.RemoteAddr
-		}
+		transaction.Message = "Dropper detected"
 
-		country := GeoMonitoring(ip)
-
-		fmt.Println("Sender IP:", host)
-		fmt.Println("Sender Country:", country)
-
-		//тут ответ отправителю-sender
+		//тут ответ отправителю
 		response := TransactionResponse{
-			Token:   MaskToken(token),
+
 			Status:  "DECLINE",
+			Token:   maskedToken,
 			Message: "Неправильный номер карты",
 		}
 
@@ -199,6 +281,24 @@ func TransactionsRecord(w http.ResponseWriter, r *http.Request) {
 
 		fmt.Println("Ошибка проведения платежа")
 	}
+
+	//тут Геомонитринг в терминал
+	country := GeoMonitoring(transaction.IP)
+
+	fmt.Println("Sender IP:", transaction.IP)
+	fmt.Println("Sender Country:", country)
+
+	// 👉 ВОТ ЭТО ДОБАВЛЯЕМ
+	geoPoints = append(geoPoints, GeoPoint{
+		Country: country,
+		IP:      transaction.IP,
+		Status:  transaction.Status,
+	})
+
+	// сохраняем транзакцию для Dashboard
+	transactions = append(transactions, transaction)
+
+	GenerateMap(geoPoints)
 }
 
 func CheckDroppers(trn Transaction) bool {
@@ -256,58 +356,75 @@ func MaskToken(token string) string {
 
 func CardData(w http.ResponseWriter, r *http.Request) {
 
-	var data ReceivedData
-
-	err := json.NewDecoder(r.Body).Decode(&data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+	pan := r.URL.Query().Get("pan")
+	if pan == "" {
+		http.Error(w, "missing pan", http.StatusBadRequest)
 		return
 	}
 
-	//тут формат карты
 	var format string
 
-	if len(data.PAN) >= 4 &&
-		(data.PAN[:4] == "5440" || data.PAN[:4] == "9860" || data.PAN[:4] == "8600") {
+	if len(pan) >= 4 &&
+		(pan[:4] == "5440" || pan[:4] == "9860" || pan[:4] == "8600") {
 		format = "KortiMilli"
 
-	} else if len(data.PAN) >= 1 &&
-		data.PAN[:1] == "4" {
+	} else if len(pan) >= 1 && pan[:1] == "4" {
 		format = "Visa"
 
-	} else if len(data.PAN) >= 2 &&
-		(data.PAN[:2] == "62" || data.PAN[:2] == "81") {
+	} else if len(pan) >= 2 &&
+		(pan[:2] == "62" || pan[:2] == "81") {
 		format = "UnionPay"
 
 	} else {
 		format = "Unknown"
 	}
 
-	//тут клиент с данными
+	// ищем клиента
+	var cardInfo CardInfo
+	found := false
+
 	for _, client := range clients {
+		if client.Card == pan {
 
-		if client.Card == data.PAN {
-
-			cardInfo := CardInfo{
+			cardInfo = CardInfo{
 				CardNumber: client.Card,
 				Format:     format,
 				ClientName: client.Name,
 				Phone:      client.MobileN,
 			}
 
-			w.Header().Set("Content-Type", "application/json")
-
-			result, err := json.MarshalIndent(cardInfo, "", "    ")
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-
-			w.Write(result)
-			return
+			found = true
+			break
 		}
 	}
-	http.Error(w, "card not found", http.StatusNotFound)
+
+	if !found {
+		http.Error(w, "card not found", http.StatusNotFound)
+		return
+	}
+
+	// история транзакций
+	history := []Transaction{}
+
+	for _, tx := range transactions {
+		if tx.PAN == pan {
+			history = append(history, tx)
+		}
+	}
+
+	// структура страницы
+	page := CardPage{
+		CardInfo: cardInfo,
+		History:  history,
+	}
+
+	tmpl, err := template.ParseFiles("templates/card.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tmpl.Execute(w, page)
 }
 
 func GeoMonitoring(ip string) string {
@@ -331,4 +448,107 @@ func GeoMonitoring(ip string) string {
 	}
 
 	return geo.Country
+}
+
+func dashboardHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./static/dashboard.html")
+}
+
+func DashboardHandler(w http.ResponseWriter, r *http.Request) {
+
+	if err := GenerateMap(geoPoints); err != nil {
+		fmt.Println("GenerateMap error:", err)
+	}
+
+	tx := transactions
+
+	// показываем только последние 5
+	if len(tx) > 5 {
+		tx = tx[len(tx)-5:]
+	}
+
+	data := Dashboard{
+		FraudScore:   78,
+		Velocity:     "HIGH",
+		Transactions: len(tx),
+		TxList:       tx,
+		GeoPoints:    geoPoints,
+		Alert:        lastAlert,
+	}
+
+	tmpl, err := template.ParseFiles("templates/dashboard.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func GenerateMap(points []GeoPoint) error {
+
+	const W = 1000
+	const H = 500
+
+	dc := gg.NewContext(W, H)
+
+	// фон (простая “карта”)
+	dc.SetRGB(0.05, 0.07, 0.12)
+	dc.Clear()
+
+	// заголовок
+	dc.SetRGB(1, 1, 1)
+	dc.DrawString("Geo Monitoring Map", 20, 30)
+
+	// рисуем точки (пока псевдо-расположение)
+	for _, p := range points {
+
+		fmt.Println("DEBUG:", p.Country, p.Status)
+
+		var x, y int
+
+		switch p.Country {
+
+		case "Tajikistan":
+			x, y = 650, 220
+
+		case "Germany":
+			x, y = 500, 170
+
+		case "USA":
+			x, y = 180, 180
+
+		case "China":
+			x, y = 760, 190
+
+		case "Russia":
+			x, y = 620, 130
+
+		case "UAE":
+			x, y = 590, 240
+
+		default:
+			x, y = 500, 250
+		}
+
+		// цвет по статусу
+		if p.Status == "SUCCESS" {
+			dc.SetRGB(0, 1, 0)
+		} else {
+			dc.SetRGB(1, 0, 0)
+		}
+
+		dc.DrawCircle(float64(x), float64(y), 8)
+		dc.Fill()
+
+		// подпись страны
+		dc.SetRGB(1, 1, 1)
+		dc.DrawString(p.Country, float64(x-20), float64(y+20))
+	}
+
+	return dc.SavePNG("static/map.png")
+
 }
